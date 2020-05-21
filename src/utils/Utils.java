42195ac62a872b44541defa172f4a19b6301590d
Fernanda Rocha <fernandaporocha@gmail.com>
2020-05-21 11:21:08 +0100

    Adding methods to generate random number within a specific range and random salary

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -3,11 +3,13 @@ package utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 public class Utils {
 
-	//The method readUserNumber will read the input of the user and return it as an int
-	//If an invalid type of data were entered by the user it will return -1
+	// The method readUserNumber will read the input of the user and return it as an
+	// int
+	// If an invalid type of data were entered by the user it will return -1
 	public static int readUserNumber() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String optionString = null;
@@ -16,9 +18,21 @@ public class Utils {
 			optionString = br.readLine();
 			option = Integer.parseInt(optionString);
 		} catch (IOException | NumberFormatException e) {
-			//If its not a number it will return -1
+			// If its not a number it will return -1
 			return -1;
 		}
 		return option;
 	}
+
+	// This method generate a random number within a specific range
+	public static int getRandom(int min, int max) {
+		Random r = new Random();
+		return min + r.nextInt(max - min);
+	}
+
+	// This method generate a random salary within a specific range
+	public static int generateRandomSalary(int min, int max) {
+		return getRandom(min, max) * 1000;
+	}
+
 }
