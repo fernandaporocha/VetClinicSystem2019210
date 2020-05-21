8c74f1853db87df02713d907dca68098ff7e2757
Fernanda Rocha <fernandaporocha@gmail.com>
2020-05-21 13:43:05 +0100

    Initializating the staff

--- src/VetClinicMain.java
+++ src/VetClinicMain.java
@@ -1,30 +1,43 @@
+import staff.StaffService;
 import utils.CLI;
 import utils.Utils;
 
 //This is the main class of the project
 public class VetClinicMain {
+	StaffService staffService;
 
-	//The constructor of the class VetClinicMain
+	// The constructor of the class VetClinicMain
 	public VetClinicMain() {
+		// It instantiates the staff service
+		staffService = new StaffService();
+
+		// It creates the medical staff
+		staffService.createMedicalStaff();
 		
+		// It creates the admin staff
+		staffService.createAdminStaff();
 	}
 
-	//Main method
+	// Main method
 	public static void main(String[] args) {
-		
-		//It prints an welcome message
+		// It creates a new instance of VetClinicMain
+		VetClinicMain vetClinic = new VetClinicMain();
+		// It prints an welcome message
 		System.out.println("Welcome to Fernanda's Vet Clinic System!");
-		//It creates the userOption variable that will keep the option chosen by the user, it starts with -1
+		// It creates the userOption variable that will keep the option chosen by the
+		// user, it starts with -1
 		int userOption = -1;
-		//It will keep showing the options, unless the user chooses the option 1 to exit.
+		// It will keep showing the options, unless the user chooses the option 1 to
+		// exit.
 		while (userOption != 0) {
-			//It prints the main options
+			// It prints the main options
 			CLI.printMainOptions();
-			//It reads the user chosen option
+			// It reads the user chosen option
 			userOption = Utils.readUserNumber();
-			
+
 		}
-		//When the user chooses option 0, it will stop the while loop and close the program.
+		// When the user chooses option 0, it will stop the while loop and close the
+		// program.
 		System.exit(0);
 
 	}
